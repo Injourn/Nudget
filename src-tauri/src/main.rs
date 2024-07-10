b27da12950086221f0da5a2eba8fb8f7dcab5c61
Injourn <thatguywhowas@live.com>
2024-07-09 20:51:37 -0700

    refactor tauri commands

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -6,38 +6,52 @@ use std::sync::Mutex;
 use rusqlite::Connection;
 
 mod commands {
-    pub(crate) mod get_transactions_in_range;
-    pub(crate) mod get_transaction;
-    pub(crate) mod get_one_transaction;
-    pub(crate) mod add_transaction;
-    pub(crate) mod remove_transaction;
-    pub(crate) mod update_transaction;
-    pub(crate) mod get_all_categories;
-    pub(crate) mod get_one_category;
-    pub(crate) mod add_category;
-    pub(crate) mod remove_category;
-    pub(crate) mod update_category;
-    pub(crate) mod get_all_budget_categories;
-    pub(crate) mod get_one_budget_category;
-    pub(crate) mod add_budget_category;
-    pub(crate) mod remove_budget_category;
-    pub(crate) mod update_budget_category;
-    pub(crate) mod get_all_budget;
-    pub(crate) mod get_one_budget;
-    pub(crate) mod add_budget;
-    pub(crate) mod remove_budget;
-    pub(crate) mod update_budget;
-    pub(crate) mod get_all_budget_plan;
-    pub(crate) mod get_one_budget_plan;
-    pub(crate) mod add_budget_plan;
-    pub(crate) mod remove_budget_plan;
-    pub(crate) mod update_budget_plan;
-    pub(crate) mod get_all_budget_budget_categories;
-    pub(crate) mod add_budget_budget_category;
-    pub(crate) mod remove_budget_budget_category;
-    pub(crate) mod get_all_budget_plan_categories;
-    pub(crate) mod add_budget_plan_category;
-    pub(crate) mod remove_budget_plan_category;
+    pub mod transactions {
+        pub(crate) mod get_transactions_in_range;
+        pub(crate) mod get_transaction;
+        pub(crate) mod get_one_transaction;
+        pub(crate) mod add_transaction;
+        pub(crate) mod remove_transaction;
+        pub(crate) mod update_transaction;
+    }
+    pub mod category {
+        pub(crate) mod get_all_categories;
+        pub(crate) mod get_one_category;
+        pub(crate) mod add_category;
+        pub(crate) mod remove_category;
+        pub(crate) mod update_category;
+    }
+    pub mod budget_category{
+        pub(crate) mod get_all_budget_categories;
+        pub(crate) mod get_one_budget_category;
+        pub(crate) mod add_budget_category;
+        pub(crate) mod remove_budget_category;
+        pub(crate) mod update_budget_category;
+    }
+    pub mod budget {
+        pub(crate) mod get_all_budget;
+        pub(crate) mod get_one_budget;
+        pub(crate) mod add_budget;
+        pub(crate) mod remove_budget;
+        pub(crate) mod update_budget;
+    }
+    pub mod budget_plan{
+        pub(crate) mod get_all_budget_plan;
+        pub(crate) mod get_one_budget_plan;
+        pub(crate) mod add_budget_plan;
+        pub(crate) mod remove_budget_plan;
+        pub(crate) mod update_budget_plan;
+    }
+    pub mod budget_budget_category{
+        pub(crate) mod get_all_budget_budget_categories;
+        pub(crate) mod add_budget_budget_category;
+        pub(crate) mod remove_budget_budget_category;
+    }
+    pub mod budget_plan_category{
+        pub(crate) mod get_all_budget_plan_categories;
+        pub(crate) mod add_budget_plan_category;
+        pub(crate) mod remove_budget_plan_category;
+    }
     pub(crate) mod get_active_budget_statistics;
 }
 mod models {
@@ -74,38 +88,38 @@ fn main() {
     tauri::Builder::default()
         .manage(Mutex::new(conn))
         .invoke_handler(tauri::generate_handler![greet,
-            crate::commands::get_transactions_in_range::get_transactions_in_range,
-            crate::commands::get_all_categories::get_all_categories,
-            crate::commands::get_one_category::get_one_category,
-            crate::commands::add_category::add_category,
-            crate::commands::remove_category::remove_category,
-            crate::commands::update_category::update_category,
-            crate::commands::get_transaction::get_transaction,
-            crate::commands::get_all_budget_categories::get_all_budget_categories,
-            crate::commands::get_one_budget_category::get_one_budget_category,
-            crate::commands::add_budget_category::add_budget_category,
-            crate::commands::remove_budget_category::remove_budget_category,
-            crate::commands::update_budget_category::update_budget_category,
-            crate::commands::get_all_budget::get_all_budget,
-            crate::commands::get_one_budget::get_one_budget,
-            crate::commands::add_budget::add_budget,
-            crate::commands::remove_budget::remove_budget,
-            crate::commands::update_budget::update_budget,
-            crate::commands::get_all_budget_plan::get_all_budget_plan,
-            crate::commands::get_one_budget_plan::get_one_budget_plan,
-            crate::commands::add_budget_plan::add_budget_plan,
-            crate::commands::get_all_budget_budget_categories::get_all_budget_budget_categories,
-            crate::commands::add_budget_budget_category::add_budget_budget_category,
-            crate::commands::remove_budget_budget_category::remove_budget_budget_category,
-            crate::commands::get_all_budget_plan_categories::get_all_budget_plan_categories,
-            crate::commands::add_budget_plan_category::add_budget_plan_category,
-            crate::commands::remove_budget_plan_category::remove_budget_plan_category,
-            crate::commands::remove_budget_plan::remove_budget_plan,
-            crate::commands::update_budget_plan::update_budget_plan,
-            crate::commands::get_one_transaction::get_one_transaction,
-            crate::commands::remove_transaction::remove_transaction,
-            crate::commands::update_transaction::update_transaction,
-            crate::commands::add_transaction::add_transaction,
+            crate::commands::transactions::get_transactions_in_range::get_transactions_in_range,
+            crate::commands::transactions::get_one_transaction::get_one_transaction,
+            crate::commands::transactions::remove_transaction::remove_transaction,
+            crate::commands::transactions::update_transaction::update_transaction,
+            crate::commands::transactions::add_transaction::add_transaction,
+            crate::commands::transactions::get_transaction::get_transaction,
+            crate::commands::category::get_all_categories::get_all_categories,
+            crate::commands::category::get_one_category::get_one_category,
+            crate::commands::category::add_category::add_category,
+            crate::commands::category::remove_category::remove_category,
+            crate::commands::category::update_category::update_category,
+            crate::commands::budget_category::get_all_budget_categories::get_all_budget_categories,
+            crate::commands::budget_category::get_one_budget_category::get_one_budget_category,
+            crate::commands::budget_category::add_budget_category::add_budget_category,
+            crate::commands::budget_category::remove_budget_category::remove_budget_category,
+            crate::commands::budget_category::update_budget_category::update_budget_category,
+            crate::commands::budget::get_all_budget::get_all_budget,
+            crate::commands::budget::get_one_budget::get_one_budget,
+            crate::commands::budget::add_budget::add_budget,
+            crate::commands::budget::remove_budget::remove_budget,
+            crate::commands::budget::update_budget::update_budget,
+            crate::commands::budget_plan::get_all_budget_plan::get_all_budget_plan,
+            crate::commands::budget_plan::get_one_budget_plan::get_one_budget_plan,
+            crate::commands::budget_plan::add_budget_plan::add_budget_plan,
+            crate::commands::budget_plan::remove_budget_plan::remove_budget_plan,
+            crate::commands::budget_plan::update_budget_plan::update_budget_plan,
+            crate::commands::budget_budget_category::get_all_budget_budget_categories::get_all_budget_budget_categories,
+            crate::commands::budget_budget_category::add_budget_budget_category::add_budget_budget_category,
+            crate::commands::budget_budget_category::remove_budget_budget_category::remove_budget_budget_category,
+            crate::commands::budget_plan_category::get_all_budget_plan_categories::get_all_budget_plan_categories,
+            crate::commands::budget_plan_category::add_budget_plan_category::add_budget_plan_category,
+            crate::commands::budget_plan_category::remove_budget_plan_category::remove_budget_plan_category,
             crate::commands::get_active_budget_statistics::get_active_budget_statistics])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
