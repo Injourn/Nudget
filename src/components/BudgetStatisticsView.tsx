3be952f57bdeb82f3a1df7bd362a9825dce0f4ff
Injourn <thatguywhowas@live.com>
2024-07-15 20:44:41 -0700

    update budget statistics view

--- src/components/BudgetStatisticsView.tsx
+++ src/components/BudgetStatisticsView.tsx
@@ -3,6 +3,8 @@ import { ReactNode, useEffect, useState } from "react";
 import callTauri from "../functions/CallTauri";
 import Table from "./ui/Table";
 import BudgetStatisticsResponseModel from "../models/BudgetStatisticsResponseModel";
+import BorderedWindow from "./ui/BorderedWindow";
+import ProgressBar from "./ui/ProgressBar";
 
 
 interface BudgetStatisticsViewProps{
@@ -12,26 +14,44 @@ interface BudgetStatisticsViewProps{
 function BudgetStatisticsView(props:BudgetStatisticsViewProps){
     const budget:BudgetModel = props.entry;
     const [budgetStatistics,setBudgetStatistics] = useState<BudgetStatisticsResponseModel[]>([]);
-    const categoryNames:string[] = ["Category","Amount budgeted","Amount remaining"];
+    const categoryNames:string[] = ["Category","Remaining","Budgeted"];
     useEffect(() => {
         callTauri<BudgetStatisticsResponseModel[]>("get_active_budget_statistics",{budget:budget}).then(categories => setBudgetStatistics(categories));
     },[props]);
     function tableRow(data:BudgetStatisticsResponseModel): ReactNode{
+        let spentNumber = Number(data.category_spent);
+        let budgetNumber = Number(data.category_budget);
         return(
             <>
-                <td>{data.category_name}</td>
+                <td>{data.category_name} <br/> <ProgressBar value={spentNumber/budgetNumber*100}/></td>
+                <td>{budgetNumber - spentNumber}</td>
                 <td>{data.category_budget}</td>
-                <td>{Number(data.category_budget) - Number(data.category_spent)}</td>
             </>
             );
     }
 
     return (
-        <Table 
-            tableRow={tableRow} 
-            tableData={budgetStatistics} 
-            columns={categoryNames} 
-            />
+        <BorderedWindow>
+            <div className="row d-none d-md-flex">
+                <div className="col-3">
+                    <img width={200} height={200}></img>
+                </div>
+                <div className="col-9">
+                    <Table 
+                        tableRow={tableRow} 
+                        tableData={budgetStatistics} 
+                        columns={categoryNames} 
+                        />
+                </div>
+            </div>
+            <div className="d-flex d-md-none">
+            <Table 
+                        tableRow={tableRow} 
+                        tableData={budgetStatistics} 
+                        columns={categoryNames} 
+                        />
+            </div>
+        </BorderedWindow>
     );
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import BudgetView from "../components/BudgetView";
 import TransactionTable from "../components/TransactionTable";
+import Calendar from "../components/ui/Calendar";
 
 
 
 function Home(){
     return(
         <>
+            <Calendar />
             <BudgetView budgetId="1"/>
             <TransactionTable />
         </>
