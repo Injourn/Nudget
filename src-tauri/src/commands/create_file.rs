76c4367ea84f0e55f6313e895352124f9d7b846d
Injourn <thatguywhowas@live.com>
2024-08-25 10:21:33 -0700

    button menu cleanup

--- src-tauri/src/commands/create_file.rs
+++ src-tauri/src/commands/create_file.rs
@@ -9,7 +9,11 @@ use crate::{database::sql_constants::SQL_BUILD, models::response::response::Resp
 
 #[tauri::command]
 pub(crate) fn create_file(conn_state: State<'_, Mutex<Connection>>, file_path: &str) -> Response<()>{
-    let conn = Connection::open(file_path);
+    let mut file_path_owned = file_path.to_owned();
+    if !file_path_owned.ends_with(".db") {
+        file_path_owned = file_path_owned + ".db";
+    }
+    let conn = Connection::open(file_path_owned);
     let mut state = conn_state.inner().lock().unwrap();
     let response = match conn {
         Ok(conn) => {
--- src/components/LoadingPage.tsx
+++ src/components/LoadingPage.tsx
@@ -5,15 +5,14 @@ import { useNavigate } from 'react-router-dom';
 function LoadingPage(){
     const navigate = useNavigate()
     return (
-        <div className="row">
-            <button type="button" className="btn btn-primary" onClick={() => open({multiple: false,
-                    
+        <div className="d-grid gap-2 d-md-flex justify-content-md-center">
+            <button type="button" className="btn btn-primary" onClick={() => open({multiple: false,  
                     filters: [{
                         name: 'Database',
                         extensions: ['db']
                     }]
                     }).then((name) => {callTauri("load_file",{filePath:name}).then(() => navigate("/home"))})}>
-                test
+                Load File
             </button>
             <button type="button" className="btn btn-primary" onClick={() => save({
                 filters: [{
@@ -21,7 +20,7 @@ function LoadingPage(){
                     extensions: ['db']
                 }]
             }).then((name) => {callTauri("create_file",{filePath:name}).then(() => navigate("/home"))})}>
-                create new
+                Create New
             </button>
         </div>
     )
